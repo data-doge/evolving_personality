d0530135ec4b34f9d0055d4ddf55b74a4c45a108
euglazer <eugenestapler@gmail.com>
2015-01-15 20:18:26 +1300

    now have sign_in and mock_sign_in helpers for tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,37 +8,39 @@ RSpec.describe PagesController, :type => :controller do
 		describe "if user signed in" do 
 				
 			before do 
-				sign_in 
+				mock_sign_in 
 				get :home
 			end 
 
-			it "returns http status of ok" do 
-				expect(response).to have_http_status(:ok)
-			end 
-
-			it "renders home" do 
-				expect(response).to render_template(:home)
-			end
+			it { expect(response).to have_http_status(:ok) }
+			it { expect(response).to render_template(:home) }
 
 		end
 
 		describe "if user not signed in" do 
 
 			before do 
-				sign_in nil
+				mock_sign_in nil
 				get :home
 			end 
 
-			it "returns http status of 302" do 
-				expect(response).to have_http_status(302)
-			end
-
-			it "redirects to new_user_session" do 
-				expect(response).to redirect_to('/users/sign_in')
-			end 
+			it { expect(response).to have_http_status(302) }
+			it { expect(response).to redirect_to('/users/sign_in') }
 
 		end 
 
 	end
+
+	describe "#questionnaire" do 
+
+		before { get :questionnaire }
+
+		it { expect(response).to have_http_status(:ok) }
+		it { expect(response).to render_template(:questionnaire) }
+
+	end 
+
+
+
 
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,6 +1,14 @@
 module ControllerMacros
+
+  def sign_in
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @current_user = User.create(email: Faker::Internet.email, password: "password")
+      sign_in @current_user
+    end 
+  end
   
-  def sign_in(user = double('user'))
+  def mock_sign_in(user = double('user'))
     if user.nil?
       allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
       allow(controller).to receive(:current_user).and_return(nil)
