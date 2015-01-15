509e83c1545e303dc070f7ece639338f89face07
euglazer <eugenestapler@gmail.com>
2015-01-16 11:52:13 +1300

    added update method to record

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,18 @@
 class Record < ActiveRecord::Base
+
+	def update!(result)
+		unless result.nil?
+			case result[:type]
+				when "E" then self.EI += result[:score] 
+				when "I" then self.EI -= result[:score]  
+				when "S" then self.SN += result[:score]
+				when "N" then self.SN -= result[:score]
+				when "T" then self.TF += result[:score]
+				when "F" then self.TF -= result[:score]
+				when "J" then self.JP += result[:score]
+				when "P" then self.JP -= result[:score]
+			end 
+		end 
+	end 
 	
 end
