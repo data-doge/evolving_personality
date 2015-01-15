c9584b9959616f54f9d7fc919c5991ed3ebc8ff0
euglazer <eugenestapler@gmail.com>
2015-01-16 12:30:41 +1300

    created skeelton for questionnaire mdoel and specs

---
+++ app/models/questionnaire.rb
@@ -0,0 +1,11 @@
+class Questionnaire
+
+	def self.create_for(user)
+
+	end
+
+	def self.update_for(user)
+
+	end
+
+end
---
+++ spec/models/questionnaire_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Question, :type => :model do
+
+	describe "#self.create_for(user)" do 
+
+		it "returns an array" do 
+		end 
+
+		it "the array's length is equal to the number of x's in the user's binary string" do 
+		end 
+
+		it "the array contains questions with uid matching the x's index in the binary string" do 
+		end 
+
+	end 
+
+	describe "#self.update_for(user)" do 
+
+		it "returns an array" do 
+		end 
+
+		it "that array contains 5 questions" do 
+		end 
+
+		it "questions are unique" do 
+		end 
+
+	end
+
+end
