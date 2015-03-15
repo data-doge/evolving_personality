5a54cdcae0ba588fa771c1a5b6691a85695f46bc
euglazer <eugenestapler@gmail.com>
2015-03-15 13:10:02 +1300

    add current user type

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,21 @@ class User < ActiveRecord::Base
 
   def binary_array
 		binary_string.split('').map { |char| char == "x" ? 4 : char.to_i }.map { |byte| (byte - 4) }
-  end 
+  end
 
   def questionnaire_complete?
   	!binary_string.include?("x")
-  end 
+  end
 
   def update_binary_string_with(answers)
     binary_string = ""
     update(binary_string: "")
     answers.each { |uid, score| binary_string << score }
-    update(binary_string: binary_string) 
-  end 
+    update(binary_string: binary_string)
+  end
+
+  def current_type
+    self.records.order(created_at: :desc).first.personality_type
+  end
 
 end
